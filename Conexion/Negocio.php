7e4c5ee5b2034e1848dc777728d438be2cb21365
root <root@debian-server.local>
2022-09-27 13:36:12 -0300

    cambios para server francisco

--- Conexion/Negocio.php
+++ Conexion/Negocio.php
@@ -15,7 +15,8 @@ header('Content-Type: application/json');
 
     public function __construct()
     {
-       $this->objetoDato = new conexion('mysql:host=localhost;dbname=bd_sb_sube','root','Fr4nc1sc0*1+Ñw');
+     //  $this->objetoDato = new conexion('mysql:host=localhost;dbname=bd_sb_sube','root','Fr4nc1sc0*1+Ñw');
+$this->objetoDato = new conexion('mysql:host=localhost;dbname=bd_sb_sube','root','fr@nc1$co');
 	$this->objetoDatoParaSP = new conexionSP('mysql:host=localhost;dbname=bd_sb_sube','root','Fr4nc1sc0*1+Ñw');
     }
 
